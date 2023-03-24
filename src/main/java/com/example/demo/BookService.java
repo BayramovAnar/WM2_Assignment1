af5f29b8d4eb884312a9549ee8018c2395973d0d
Anar Bayramov <bayramovanar1703@gmail.com>
2023-03-24 15:12:43 +0400

    book service added

---
+++ src/main/java/com/example/demo/BookService.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks(){
+        return bookRepository.findAll();
+    }
+
+    public Book createBook(Book book){
+        return bookRepository.save(book);
+    }
+
+    public Book updateBook(Book book){
+        return  bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id){
+        bookRepository.deleteById(id);
+    }
+
+    public List<Book> getBooksByAuthor(Long authorId){
+        return bookRepository.findByAuthorId(authorId);
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findBookById(bookId);
+    }
+}
