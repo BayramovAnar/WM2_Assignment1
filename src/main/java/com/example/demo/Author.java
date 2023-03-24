3afe3702f3e3766b57687355f0ff90c54c859b96
Anar Bayramov <bayramovanar1703@gmail.com>
2023-03-24 14:51:43 +0400

    setting up

---
+++ src/main/java/com/example/demo/Author.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "author")
+    private Set<Book> books;
+
+    public Author() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Author(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
